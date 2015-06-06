7d7fe79667628ea26e9cc62b50fac337f41247aa
Chris Trotman <chris@trotman.io>
2015-06-06 22:00:21 +1000

    Use the babel runtime polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /(node_modules|bower_components)/,
-      loader: 'babel'
+      loader: 'babel?optional[]=runtime'
     }]
   }
 };
